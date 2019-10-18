b3f2a1f9ca25119dca1390fc7cfd3ccec9b88d09
lwhay <lwh@whu.edu.cn>
2019-10-18 12:08:00 -0700

    Disalbe autoupdate and unorderedmap.

--- tests/CF2_Unit/Folly_Usage.cpp
+++ tests/CF2_Unit/Folly_Usage.cpp
@@ -11,15 +11,24 @@
 #include "folly/AtomicHashMap.h"
 #include "folly/AtomicUnorderedMap.h"
 
-TEST(FollyTest, UnitOperation) {
+TEST(FollyTest, AtomicHashMapOperation) {
     folly::AtomicHashMap<uint64_t, uint64_t> fmap(128);
     fmap.insert(1, 1);
     ASSERT_EQ(fmap.find(1)->second, 1);
     fmap.insert(1, 2);
-    ASSERT_EQ(fmap.find(1)->second, 2);
-    fmap.erase(1);
     ASSERT_EQ(fmap.find(1)->second, 1);
+    fmap.erase(1);
+    auto ret = fmap.find(1);
+    //ASSERT_ANY_THROW(fmap.find(1));
 }
+
+/*TEST(FollyTest, AtomicUnorderedMapOperation) {
+    folly::AtomicUnorderedInsertMap<uint64_t, uint64_t> fmap(128);
+    fmap.findOrConstruct(1, 1);
+    ASSERT_EQ(fmap.find(1)->second, 1);
+    fmap.emplace(1, 2);
+    ASSERT_EQ(fmap.find(1)->second, 1);
+}*/
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
