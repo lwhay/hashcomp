19f327f6d03e7e0b5d90e542079f2288b8ad695b
lwhay <lwh@whu.edu.cn>
2019-10-26 18:36:42 +0800

    Print total keys/counts.

--- stream/tests/QuantilePointGenerator.cpp
+++ stream/tests/QuantilePointGenerator.cpp
@@ -20,6 +20,7 @@ int main(int argc, char **argv) {
         qt.add(make_pair(std::atol(fields[0].c_str()), std::atol(fields[1].c_str())));
     }
     for (double c = 10000; c <= 60000; c += 10000) cout << c << ": " << qt.getAccCount(c / qt.getKeyCount()) << endl;
+    cout << "Total keys: " << qt.getKeyCount() << " total loads: " << qt.getAccCount() << endl;
     for (double r = 0.1; r <= 1.0; r += 0.1) cout << r << ": " << qt.getKeyCount(r) << endl;
     input.close();
     return 0;
