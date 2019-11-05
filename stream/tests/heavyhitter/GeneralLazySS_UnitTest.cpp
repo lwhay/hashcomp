f0bccd810464e8ca08b143496e9fe6717185b36a
Michael <lwhaywhu@gmail.com>
2019-11-05 23:51:52 +0800

    isolated parallel GLSS test case.

--- stream/tests/heavyhitter/GeneralLazySS_UnitTest.cpp
+++ stream/tests/heavyhitter/GeneralLazySS_UnitTest.cpp
@@ -2,9 +2,11 @@
 // Created by Michael on 11/2/19.
 //
 
+#include <thread>
 #include <unordered_map>
 #include "gtest/gtest.h"
 #include "generator.h"
+#include "LazySpaceSaving.h"
 #include "GeneralLazySS.h"
 
 constexpr static int count_per_round = 100000;
@@ -58,6 +60,54 @@ TEST(GeneralLazySSTest, uintvTest) {
     funcCall<uint16_t>();
     funcCall<uint32_t>();
     funcCall<uint64_t>();
+}
+
+constexpr int thread_number = 4;
+constexpr uint64_t itemsize = (1llu << 24);
+constexpr double fPhi = 0.0001;
+
+int actualmaxcount[thread_number];
+
+TEST(GeneralLazySSTest, dummyTest) {
+    GeneralLazySS<uint64_t> glss(0.0001);
+    for (uint64_t c = 0; c < itemsize; c++) {
+        glss.put(c);
+        glss.put(13332, 1);
+    }
+    Item<uint64_t> *output = glss.output(true);
+    ASSERT_EQ(output[1].getCount(), itemsize + 1);
+}
+
+void workerThread(GeneralLazySS<uint64_t> glss, int tid, bool reverse) {
+    if (reverse) {
+        for (uint64_t c = itemsize - 1; c != std::numeric_limits<uint64_t>::max(); c--) {
+            glss.put(c);
+            glss.put(13332);
+        }
+    } else {
+        for (uint64_t c = 0; c < itemsize; c++) {
+            glss.put(c);
+            glss.put(13332);
+        }
+    }
+    actualmaxcount[tid] = glss.output(true)[1].getCount();
+}
+
+TEST(GeneralLazySSTest, parallelTest) {
+    std::vector<std::thread> workers;
+    GeneralLazySS<uint64_t> **glsses = (GeneralLazySS<uint64_t> **) std::malloc(sizeof(GeneralLazySS<uint64_t> *) * 4);
+    int tids[thread_number];
+    for (int t = 0; t < thread_number; t++) {
+        glsses[t] = new GeneralLazySS<uint64_t>(fPhi);
+        tids[t] = t;
+        workers.push_back(std::thread(workerThread, std::ref(*glsses[t]), tids[t], false));
+    }
+    for (int t = 0; t < thread_number; t++) {
+        workers[t].join();
+        std::free(glsses[t]);
+        ASSERT_EQ(actualmaxcount[t], itemsize + 1);
+    }
+    delete[] glsses;
 }
 
 int main(int argc, char **argv) {
