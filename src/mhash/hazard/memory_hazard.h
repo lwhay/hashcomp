ddd5c395907cc9d6131fc578dd0b39167e08fc24
lwhay <lwh@whu.edu.cn>
2019-11-24 07:43:30 -0800

    add simple hazard.

---
+++ src/mhash/hazard/memory_hazard.h
@@ -0,0 +1,56 @@
+//
+// Created by iclab on 11/24/19.
+//
+
+#ifndef HASHCOMP_MEMORY_HAZARD_H
+#define HASHCOMP_MEMORY_HAZARD_H
+
+#include <atomic>
+#include <cassert>
+
+constexpr size_t thread_limit = (1 << 8);
+
+struct holder {
+    alignas(64) std::atomic<uint64_t> address;
+public:
+    void init() { address.store(0); }
+
+    void store(uint64_t ptr) { address.store(ptr); }
+
+    uint64_t load() { return address.load(); }
+};
+
+class memory_hazard {
+private:
+    holder holders[thread_limit];
+    size_t thread_number = 0;
+
+public:
+    void registerThread() {
+        holders[thread_number++].init();
+    }
+
+    uint64_t load(size_t tid, std::atomic<uint64_t> &ptr) {
+        holders[tid].store(ptr.load());
+        return holders[tid].load();
+    }
+
+    void read(size_t tid) { holders[tid].store(0); }
+
+    bool free(uint64_t ptr) {
+        assert(ptr != 0);
+        bool busy = false;
+        for (size_t t = 0; t < thread_number; t++) {
+            if (holders[t].load() == (uint64_t) ptr) {
+                busy = true;
+            }
+        }
+        if (busy) return false;
+        else {
+            std::free((void *) ptr);
+            return true;
+        }
+    }
+};
+
+#endif //HASHCOMP_MEMORY_HAZARD_H
---
+++ tests/MH_Efficiency/HazardDummyTest.cpp
@@ -0,0 +1,143 @@
+//
+// Created by iclab on 11/24/19.
+//
+
+#include <iostream>
+#include <thread>
+#include "memory_hazard.h"
+#include "tracer.h"
+
+struct node {
+    uint64_t key;
+    uint64_t value;
+};
+
+size_t align_width = (1 << 6);
+
+size_t list_volume = (1 << 20);
+
+size_t thrd_number = (1 << 3);
+
+size_t total_count = (1 << 20);
+
+atomic<int> stopMeasure(0);
+
+size_t worker_gran = thrd_number / 2;
+
+memory_hazard *deallocator;
+
+long *runtime;
+
+size_t *operations;
+
+void reader(std::atomic<uint64_t> *bucket, size_t tid) {
+    uint64_t total = 0;
+    Tracer tracer;
+    tracer.startTime();
+    while (stopMeasure.load() == 0) {
+        for (size_t i = 0; i < total_count / worker_gran; i++) {
+            size_t idx = i % (list_volume / align_width) * align_width;
+            node *ptr = (node *) deallocator->load(tid, std::ref(bucket[idx]));
+            total += ptr->value;
+            deallocator->read(tid);
+            //std::cout << "r" << tid << i << std::endl;
+        }
+    }
+    runtime[tid] = tracer.getRunTime();
+    operations[tid] = total;
+}
+
+void init(std::atomic<uint64_t> *bucket) {
+    for (size_t i = 0; i < total_count / worker_gran; i++) {
+        node *ptr = (node *) std::malloc(sizeof(node));
+        size_t idx = i % (list_volume / align_width) * align_width;
+        ptr->key = idx;
+        ptr->value = 1;
+        bucket[idx].store((uint64_t) ptr);
+    }
+}
+
+void print(std::atomic<uint64_t> *bucket) {
+    for (size_t i = 0; i < 64; i++) {
+        size_t idx = i % (list_volume / align_width) * align_width;
+        node *ptr = (node *) bucket[idx].load();
+        std::cout << ptr << " " << ptr->key << " " << ptr->value << std::endl;
+    }
+}
+
+void writer(std::atomic<uint64_t> *bucket, size_t tid) {
+    uint64_t total = 0;
+    Tracer tracer;
+    tracer.startTime();
+    while (stopMeasure.load() == 0) {
+        for (size_t i = 0; i < total_count / worker_gran; i++) {
+            node *ptr = (node *) std::malloc(sizeof(node));
+            ptr->key = i;
+            ptr->value = 1;
+            uint64_t old;
+            size_t idx = i % (list_volume / align_width) * align_width;
+            do {
+                old = bucket[idx].load();
+            } while (!bucket[idx].compare_exchange_strong(old, (uint64_t) ptr));
+            while (!deallocator->free(old));
+            total++;
+        }
+    }
+    runtime[tid] = tracer.getRunTime();
+    operations[tid] = total;
+}
+
+int main(int argc, char **argv) {
+    if (argc == 5) {
+        align_width = std::atol(argv[1]);
+        list_volume = std::atol(argv[2]);
+        thrd_number = std::atol(argv[3]);
+        total_count = std::atol(argv[4]);
+    }
+    std::atomic<uint64_t> *bucket = new std::atomic<uint64_t>[list_volume];
+    runtime = new long[thrd_number];
+    operations = new uint64_t[thrd_number];
+    init(bucket);
+    //print(bucket);
+    Tracer tracer;
+    tracer.startTime();
+    std::vector<std::thread> workers;
+    deallocator = new memory_hazard;
+    Timer timer;
+    timer.start();
+    size_t t = 0;
+    for (; t < worker_gran; t++) {
+        deallocator->registerThread();
+        workers.push_back(std::thread(reader, bucket, t));
+    }
+    for (; t < thrd_number; t++) {
+        workers.push_back(std::thread(writer, bucket, t));
+    }
+    while (timer.elapsedSeconds() < 3) {
+        sleep(1);
+    }
+    stopMeasure.store(1, memory_order_relaxed);
+    long readtime = 0;
+    long writetime = 0;
+    size_t readcount = 0;
+    size_t writecount = 0;
+    for (size_t t = 0; t < thrd_number; t++) {
+        workers[t].join();
+        if (t < worker_gran) {
+            readtime += runtime[t];
+            readcount += operations[t];
+            std::cout << "\tReader" << t << ": " << operations[t] << "\t" << runtime[t] << std::endl;
+        } else {
+            writetime += runtime[t];
+            writecount += operations[t];
+            std::cout << "\tWriter" << t - worker_gran << ": " << operations[t] << "\t" << runtime[t] << std::endl;
+        }
+    }
+    double readthp = (double) readcount * worker_gran / readtime;
+    double writethp = (double) writecount * worker_gran / writetime;
+    std::cout << "Total time: " << tracer.getRunTime() << " rthp: " << readthp << " wthp: " << writethp << std::endl;
+    delete[] bucket;
+    delete[] runtime;
+    delete[] operations;
+    return 0;
+}
