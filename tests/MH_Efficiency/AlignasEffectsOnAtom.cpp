315cd3c9f333a40ab4aca07ecba55197ddbf1e8d
lwhay <lwh@whu.edu.cn>
2019-11-24 19:36:05 -0800

    separate vs diperse atomic aligns.

--- tests/MH_Efficiency/AlignasEffectsOnAtom.cpp
+++ tests/MH_Efficiency/AlignasEffectsOnAtom.cpp
@@ -16,58 +16,92 @@ size_t total_count = (1 << 20);
 
 constexpr size_t default_align = (1 << 6);
 
-void casworker(std::atomic<uint64_t> *pcon, size_t tid) {
+template<typename T>
+void casworker(T *pcon, size_t tid, bool loc = true, uint64_t aln = 1) {
     for (size_t i = 0; i < total_count / thrd_number; i++) {
         uint64_t d = pcon->load();
-        while (!pcon->compare_exchange_strong(d, i));
+        if (loc) while (!pcon->compare_exchange_strong(d, i));
+        else while (!(pcon + i % thrd_number * aln)->compare_exchange_strong(d, i));
     }
 }
 
-void addworker(std::atomic<uint64_t> *pcon, size_t tid) {
+template<typename T>
+void addworker(T *pcon, size_t tid, bool loc = true, uint64_t aln = 1) {
     for (size_t i = 0; i < total_count / thrd_number; i++) {
-        while (!pcon->fetch_add(1));
+        if (loc) while (!pcon->fetch_add(1));
+        else while (!(pcon + i % thrd_number * aln)->fetch_add(1));
     }
 }
 
-void loadworker(std::atomic<uint64_t> *pcon, size_t tid) {
+template<typename T>
+void loadworker(T *pcon, size_t tid, bool loc = true, uint64_t aln = 1) {
     for (size_t i = 0; i < total_count / thrd_number; i++) {
-        pcon->load();
+        if (loc) pcon->load();
+        else (pcon + i % thrd_number * aln)->load();
     }
 }
 
-void storeworker(std::atomic<uint64_t> *pcon, size_t tid) {
+template<typename T>
+void storeworker(T *pcon, size_t tid, bool loc = true, uint64_t aln = 1) {
     for (size_t i = 0; i < total_count / thrd_number; i++) {
-        pcon->store(i);
+        if (loc) pcon->store(i);
+        else (pcon + i % thrd_number * aln)->store(i);
     }
 }
 
+template<typename T>
+void readworker(T *acon, size_t tid, bool loc = true, uint64_t aln = 1) {
+    for (size_t i = 0; i < total_count / thrd_number; i++) {
+        if (loc) uint64_t v = *acon;
+        else uint64_t v = *(acon + i % thrd_number * aln);
+    }
+}
+
+template<typename T>
+void writeworker(T *acon, size_t tid, bool loc = true, uint64_t aln = 1) {
+    for (size_t i = 0; i < total_count / thrd_number; i++) {
+        if (loc) *acon = i;
+        else *(acon + i % thrd_number * aln) = i;
+    }
+}
+
+template<typename T>
 struct align_controller {
-    alignas(default_align) std::atomic<uint64_t> controller;
+    alignas(default_align) T controller;
 };
 
-void run(void (*func)(std::atomic<uint64_t> *, size_t), char *name) {
+template<typename T>
+void run(void (*func)(T *, size_t, bool, uint64_t), char *name, bool loc = true) {
     std::cout << name << std::endl;
     std::vector<std::thread> threads;
     Tracer tracer;
-    alignas(default_align) std::atomic<uint64_t> controller[thrd_number];
+
+    alignas(default_align) T controller[thrd_number];
     tracer.startTime();
-    for (size_t t = 0; t < thrd_number; t++) threads.push_back(std::thread(func, controller + t, t));
+    for (size_t t = 0; t < thrd_number; t++)
+        if (loc) threads.push_back(std::thread(func, controller + t, t, loc, 1));
+        else threads.push_back(std::thread(func, controller + 0, t, loc, 1));
     for (size_t t = 0; t < thrd_number; t++) threads[t].join();
-    std::cout << "\taligns: " << (double) total_count / tracer.getRunTime() << std::endl;
+    std::cout << ((loc) ? "\taligns: " : "\talignd: ") << (double) total_count / tracer.getRunTime() << std::endl;
     threads.clear();
 
-    align_controller aontroller[thrd_number];
-    tracer.startTime();
-    for (size_t t = 0; t < thrd_number; t++) threads.push_back(std::thread(func, &aontroller[t].controller, t));
-    for (size_t t = 0; t < thrd_number; t++) threads[t].join();
-    std::cout << "\talignas: " << (double) total_count / tracer.getRunTime() << std::endl;
-    threads.clear();
+    if (loc) {
+        align_controller<T> aontroller[thrd_number];
+        tracer.startTime();
+        for (size_t t = 0; t < thrd_number; t++)
+            threads.push_back(std::thread(func, &aontroller[t].controller, t, loc, 1));
+        for (size_t t = 0; t < thrd_number; t++) threads[t].join();
+        std::cout << "\talignas: " << (double) total_count / tracer.getRunTime() << std::endl;
+        threads.clear();
+    }
 
-    std::atomic<uint64_t> *sontroller = new std::atomic<uint64_t>[align_width * thrd_number];
+    T *sontroller = new T[align_width * thrd_number];
     tracer.getRunTime();
-    for (size_t t = 0; t < thrd_number; t++) threads.push_back(std::thread(func, sontroller + t * align_width, t));
+    for (size_t t = 0; t < thrd_number; t++)
+        if (loc) threads.push_back(std::thread(func, sontroller + t * align_width, t, loc, align_width));
+        else threads.push_back(std::thread(func, sontroller, t, loc, align_width));
     for (size_t t = 0; t < thrd_number; t++) threads[t].join();
-    std::cout << "\tseparate: " << (double) total_count / tracer.getRunTime() << std::endl;
+    std::cout << ((loc) ? "\tseparate: " : "\tdisperse: ") << (double) total_count / tracer.getRunTime() << std::endl;
     delete[] sontroller;
 }
 
@@ -77,9 +111,23 @@ int main(int argc, char **argv) {
         thrd_number = std::atol(argv[2]);
         total_count = std::atol(argv[3]);
     }
-    run(casworker, "cas");
-    run(addworker, "add");
-    run(storeworker, "store");
-    run(loadworker, "load");
+    std::cout << "align: " << align_width << " thrd: " << thrd_number << " total: " << total_count << std::endl;
+    run<std::atomic<uint64_t>>(casworker, "cas", true);
+    run<std::atomic<uint64_t>>(addworker, "add", true);
+    run<std::atomic<uint64_t>>(storeworker, "store", true);
+    run<std::atomic<uint64_t>>(loadworker, "load", true);
+    run<std::atomic<uint64_t>>(writeworker, "sstore", true);
+    run<std::atomic<uint64_t>>(readworker, "sload", true);
+    run<uint64_t>(readworker, "read", true);
+    run<uint64_t>(writeworker, "write", true);
+
+    run<std::atomic<uint64_t>>(casworker, "wcas", false);
+    run<std::atomic<uint64_t>>(addworker, "wadd", false);
+    run<std::atomic<uint64_t>>(storeworker, "wstore", false);
+    run<std::atomic<uint64_t>>(loadworker, "wload", false);
+    run<std::atomic<uint64_t>>(writeworker, "wsstore", false);
+    run<std::atomic<uint64_t>>(readworker, "wsload", false);
+    run<uint64_t>(readworker, "wread", false);
+    run<uint64_t>(writeworker, "wwrite", false);
     return 0;
 }
