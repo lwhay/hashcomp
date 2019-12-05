e07846d2c8d2aa7f65f699a45a5be2b6d66851a2
lwhay <lwh@whu.edu.cn>
2019-12-05 09:32:52 -0800

    stepping atomic load.

--- tests/AWL_Unit/awlmap_Usage.cpp
+++ tests/AWL_Unit/awlmap_Usage.cpp
@@ -2,13 +2,31 @@
 // Created by iclab on 11/28/19.
 //
 
-#include <cstdint>
+#include <atomic>
 #include <cstring>
 #include <deque>
 #include <functional>
 #include <thread>
 #include "gtest/gtest.h"
 #include "concurrent_hash_map.h"
+
+TEST(awlmapTests, UnionTest) {
+    struct dummy {
+        union {
+            uint64_t body;
+            std::atomic<uint64_t> ctrl;
+        };
+    };
+    dummy dum{0};
+    ASSERT_EQ(dum.body, 0);
+    ASSERT_EQ(dum.ctrl.load(), 0);
+    dum.ctrl.store(1);
+    ASSERT_EQ(dum.body, 1);
+    ASSERT_EQ(dum.ctrl.load(), 1);
+    dum.body = 0;
+    ASSERT_EQ(dum.body, 0);
+    ASSERT_EQ(dum.ctrl.load(), 0);
+}
 
 TEST(awlmapTests, UnitOperations) {
     ConcurrentHashMap<int, int, std::hash<int>, std::equal_to<>> map(16, 3, 1);
---
+++ utils/CacheSteppingAtomicTest.cpp
@@ -0,0 +1,74 @@
+//
+// Created by iclab on 12/3/19.
+//
+
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include "tracer.h"
+
+using namespace std;
+
+size_t step = (1llu << 20);
+size_t total = (1llu << 30);
+size_t degree = (1llu << 2);
+size_t iteration = (1llu << 4);
+
+atomic<double> *workloads;
+
+void generate() {
+    workloads = new atomic<double>[total];
+    std::default_random_engine engine(static_cast<double>(chrono::steady_clock::now().time_since_epoch().count()));
+    std::uniform_int_distribution<size_t> dis(0, std::numeric_limits<uint32_t>::max());
+    Tracer tracer;
+    tracer.startTime();
+    for (size_t i = 0; i < total; i++) {
+        workloads[i].store(static_cast<double>(dis(engine)) / std::numeric_limits<uint32_t>::max());
+    }
+    cout << tracer.getRunTime() << endl;
+}
+
+void pcompute() {
+    std::thread threads[degree];
+    double avg = 0;
+    double sum[degree];
+    long times[degree];
+    long time;
+    Tracer tracer;
+    tracer.getRunTime();
+    for (size_t t = 0; t < degree; t++) {
+        sum[t] = 0;
+        threads[t] = std::thread([](size_t tid, double &stat, long &time) {
+            Tracer tracer;
+            tracer.startTime();
+            for (size_t r = 0; r < iteration; r++)
+                for (size_t i = tid; i < total / degree; i += degree)
+                    stat += workloads[((i + tid) * step) % total].load();
+            time = tracer.getRunTime();
+        }, t, std::ref(sum[t]), std::ref(times[t]));
+    }
+    for (size_t t = 0; t < degree; t++) {
+        threads[t].join();
+        avg += sum[t];
+        time += times[t];
+    }
+    cout << tracer.getRunTime() << endl;
+    cout << "\t" << avg / degree << " " << total * degree / time << endl;
+}
+
+void finished() {
+    delete[] workloads;
+}
+
+int main(int argc, char **argv) {
+    if (argc > 4) {
+        degree = std::atol(argv[1]);
+        total = std::atol(argv[2]);
+        step = std::atol(argv[3]);
+        iteration = std::atol(argv[4]);
+    }
+    generate();
+    pcompute();
+    return 0;
+}
