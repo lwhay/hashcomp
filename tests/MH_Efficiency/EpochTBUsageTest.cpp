453d9841fc738385d8287975baa2b5923b64508e
lwhay <lwh@whu.edu.cn>
2020-01-11 09:06:45 -0800

    addressing bzhazard memory issues.

--- tests/MH_Efficiency/EpochTBUsageTest.cpp
+++ tests/MH_Efficiency/EpochTBUsageTest.cpp
@@ -19,9 +19,9 @@
 
 const int NUM_THREADS = 1;
 
-const int NUM_ROUNDS = (1 << 20); // For tb_hazard, number of rounds should be larger than that of elements.
+const int NUM_ROUNDS = (1 << 30); // For tb_hazard, number of rounds should be larger than that of elements.
 
-const int NUM_ELEMENTS = (1 << 8/*19*/);
+const int NUM_ELEMENTS = (1 << 4/*19*/);
 
 // Rather than bz_hazard, other reclaimers are dummy classes in case of (un)protect
 template<typename type, template<typename, typename> class tb_reclaimer>
@@ -57,45 +57,50 @@ public:
 };
 
 void tb_hazard_original() {
-    typedef reclaimer_hazardptr<uint64_t, pool_perthread_and_shared<uint64_t, allocator_new<uint64_t>>> Reclaimer;
-    typedef allocator_new<uint64_t> Allocator;
-    typedef pool_perthread_and_shared<uint64_t, allocator_new<uint64_t>> Pool;
+    typedef reclaimer_hazardptr<uint64_t, pool_perthread_and_shared<uint64_t, allocator_once<uint64_t>>> Reclaimer;
+    typedef allocator_once<uint64_t> Allocator;
+    typedef pool_perthread_and_shared<uint64_t, allocator_once<uint64_t>> Pool;
 
     Allocator *alloc = new Allocator(NUM_THREADS, nullptr);
     Pool *pool = new Pool(NUM_THREADS, alloc, nullptr);
     Reclaimer *reclaimer = new Reclaimer(NUM_THREADS, pool, nullptr);
-    record_manager<Reclaimer, Allocator, Pool, uint64_t> *recordManager = new record_manager<Reclaimer, Allocator, Pool, uint64_t>(
-            NUM_THREADS);
+    /*record_manager<Reclaimer, Allocator, Pool, uint64_t> *recordManager = new record_manager<Reclaimer, Allocator, Pool, uint64_t>(
+            NUM_THREADS);*/
     Tracer tracer;
     tracer.startTime();
-    recordManager->initThread(0);
+    //recordManager->initThread(0);
 
     uint64_t *cache[NUM_ELEMENTS];
 
-    for (int i = 0; i < NUM_ELEMENTS; i++) cache[i] = alloc->allocate(0);
     std::cout << "Reclaimer allocate: " << tracer.getRunTime() << std::endl;
     for (int k = 0; k < NUM_ROUNDS; k++) {
+        for (int i = 0; i < NUM_ELEMENTS; i++) cache[i] = alloc->allocate(0);
         for (int i = 0; i < NUM_ELEMENTS; i++) reclaimer->protect(0, cache[i], callbackReturnTrue, nullptr, false);
 
         for (int i = 0; i < NUM_ELEMENTS; i++) reclaimer->unprotect(0, cache[i]);
 
-        //for (int i = 0; i < NUM_ELEMENTS; i++) reclaimer->retire(0, cache[i]);
+        for (int i = 0; i < NUM_ELEMENTS; i++) {
+            reclaimer->retire(0, cache[i]);
+            alloc->deallocate(0, cache[i]);
+        }
+        if (k % 10000000 == 0) {
+            std::cout << pool->computeSize(0) << std::endl;
+        }
     }
     std::cout << "Reclaimer (un)protect: " << tracer.getRunTime() << std::endl;
-    for (int i = 0; i < NUM_ELEMENTS; i++) alloc->deallocate(0, cache[i]);
 
     std::cout << "Reclaimer deallocate: " << tracer.getRunTime() << std::endl;
-    recordManager->deinitThread(0);
+    /*recordManager->deinitThread(0);
     std::cout << "Reclaimer deinitThread: " << tracer.getRunTime() << std::endl;
-    delete recordManager;
+    delete recordManager;*/
     //delete reclaimer;
-    delete pool;
+    //delete pool;
     delete alloc;
 }
 
 int main(int argc, char **argv) {
     tb_hazard_original();
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 0; i++) {
         testcase<uint64_t, reclaimer_debra> debra;
         debra.bz_test();
         testcase<uint64_t, reclaimer_debraplus> debraplus;
