4d2049e62521dd1f224b5fab230af87efd1fadcb
lwhay <lwhay@whu.edu.cn>
2020-02-24 06:00:22 +0800

    different hashers unittest.

--- tests/CF2_Unit/CUCKOO_Usage.cpp
+++ tests/CF2_Unit/CUCKOO_Usage.cpp
@@ -10,6 +10,73 @@
 #include "gtest/gtest.h"
 #include "libcuckoo/cuckoohash_map.hh"
 
+#define BIG_CONSTANT(x) (x##LLU)
+
+uint64_t MurmurHash64A(const void *key, int len, uint64_t seed) {
+    const uint64_t m = BIG_CONSTANT(0xc6a4a7935bd1e995);
+    const int r = 47;
+
+    uint64_t h = seed ^(len * m);
+
+    const uint64_t *data = (const uint64_t *) key;
+    const uint64_t *end = data + (len / 8);
+
+    while (data != end) {
+        uint64_t k = *data++;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h ^= k;
+        h *= m;
+    }
+
+    const unsigned char *data2 = (const unsigned char *) data;
+
+    switch (len & 7) {
+        case 7:
+            h ^= uint64_t(data2[6]) << 48;
+        case 6:
+            h ^= uint64_t(data2[5]) << 40;
+        case 5:
+            h ^= uint64_t(data2[4]) << 32;
+        case 4:
+            h ^= uint64_t(data2[3]) << 24;
+        case 3:
+            h ^= uint64_t(data2[2]) << 16;
+        case 2:
+            h ^= uint64_t(data2[1]) << 8;
+        case 1:
+            h ^= uint64_t(data2[0]);
+            h *= m;
+    };
+
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
+}
+
+TEST(CuckooTests, HasherDetail) {
+    char *tc = "wpoeiruwepoiruewir";
+    std::hash<char *> hasher;
+    std::hash<uint64_t> hashei;
+    std::cout << hasher(tc) << " " << (uint64_t) tc << " " << hashei((uint64_t) tc) << std::endl;
+    std::string ts(tc);
+    std::hash<std::string> hashes;
+    char *tt = new char[std::strlen(tc)];
+    std::strcpy(tt, tc);
+    std::hash<const char *> hasheu;
+    std::string td(tc);
+    std::cout << hashes(ts) << " " << hashes(td) << std::endl;
+    std::cout << hasheu(ts.c_str()) << " " << hasher(tt) << " " << hasher(tc) << std::endl;
+    std::cout << MurmurHash64A((void *) ts.c_str(), std::strlen(ts.c_str()), 23) << " "
+              << MurmurHash64A((void *) tc, std::strlen(tc), 23) << " "
+              << MurmurHash64A((void *) tt, std::strlen(tt), 23) << std::endl;
+}
+
 TEST(CuckooTests, UnitOperations) {
     libcuckoo::cuckoohash_map<uint64_t, uint64_t, std::hash<uint64_t>, std::equal_to<uint64_t>,
             std::allocator<std::pair<const uint64_t, uint64_t>>, 8> cmap(128);
