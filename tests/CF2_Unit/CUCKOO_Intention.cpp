de57fc59484ee9c79804b3b9e30a39aa9a650acb
lwhay <lwh@whu.edu.cn>
2020-02-29 21:38:17 -0800

    multicuckoo test

--- tests/CF2_Unit/CUCKOO_Intention.cpp
+++ tests/CF2_Unit/CUCKOO_Intention.cpp
@@ -63,52 +63,72 @@ uint64_t MurmurHash64A(const void *key, int len, uint64_t seed) {
 
 constexpr uint64_t esize = 10000000;
 constexpr double eta = 0.5;
-constexpr uint64_t dim = 2;
-constexpr uint64_t hashseeds[dim] = {15126133284972304873llu, 2398702347823893llu};
+constexpr uint64_t dim = 2; //4;
+constexpr uint64_t secondratio = 1;
+constexpr uint64_t hashseeds[dim] = {15126133284972304873llu,
+                                     2398702347823893llu}; //, 7343232432432323234llu, 18342323893231224987llu};
 constexpr uint64_t tsize = (uint64_t) ((1 + eta) * esize);
+constexpr uint64_t tablesize[dim] = {tsize, tsize / secondratio}; //, tsize / secondratio, tsize / secondratio};
 constexpr uint64_t tries = 64;
 constexpr std::hash<uint64_t> hasher;
 
-bool tableinsert(uint64_t *key, uint64_t *hashtable, uint64_t seed) {
-    uint64_t hash = MurmurHash64A(key, std::strlen((char *) key), seed) % tsize;
+bool tableinsert(uint64_t *key, uint64_t *hashtable, uint64_t seed, uint64_t ts) {
+    uint64_t hash = MurmurHash64A(key, std::strlen((char *) key), seed) % ts;
     //std::cout << hash << " ";
     uint64_t tmp = *key;
     *key = hashtable[hash];
     hashtable[hash] = tmp;
-    return (*key == 0);
+    //return true;
+    return (*key == -1);
 }
 
 TEST(CUCKOOIntention, NaiveTest) {
     uint64_t *keys = new uint64_t[esize];
-    uint64_t *twintables[dim];
-    twintables[0] = new uint64_t[tsize];
-    twintables[1] = new uint64_t[tsize];
+    uint64_t *tables[dim];
+    tables[0] = new uint64_t[tsize];
+    for (int i = 1; i < dim; i++) tables[i] = new uint64_t[tsize / secondratio];
     std::memset(keys, 0, esize * sizeof(uint64_t));
-    std::memset(twintables[0], 0, tsize * sizeof(uint64_t));
-    std::memset(twintables[1], 0, tsize * sizeof(uint64_t));
+    std::memset(tables[0], -1, tsize * sizeof(uint64_t));
+    for (int i = 1; i < dim; i++) std::memset(tables[i], -1, tsize / secondratio * sizeof(uint64_t));
     uint64_t cache[dim];
     std::memset(cache, 0, dim * sizeof(uint64_t));
     size_t totaltries = 0;
     for (int i = 0; i < esize; i++) {
         keys[i] = i;
-        uint64_t curkey = keys[i];
         uint64_t curtry = 0;
         cache[0] = keys[i];
         do {
             curtry++;
         } while (curtry < tries &&
-                 !tableinsert(cache, twintables[(curtry - 1) % dim], hashseeds[(curtry - 1) % dim]));
+                 !tableinsert(cache, tables[(i + curtry - 1) % dim], hashseeds[(i + curtry - 1) % dim],
+                              tablesize[(i + curtry - 1) % dim]));
         //std::cout << std::endl;
 #if 0
+        uint64_t curkey = keys[i];
         std::cout << i << " " << curkey << " " << curtry << " "
                   << MurmurHash64A(&keys[i], std::strlen((char *) &keys[i]), hashseeds[0]) % tsize << std::endl;
 #endif
         totaltries += curtry;
     }
-    delete[] twintables[0];
-    delete[] twintables[1];
+    uint64_t hits[dim];
+    std::memset(hits, 0, sizeof(uint64_t) * dim);
+    for (int i = 0; i < tsize; i++) {
+        if (tables[0][i] != -1) hits[0]++;
+    }
+    for (int d = 1; d < dim; d++)
+        for (int i = 0; i < tsize / secondratio; i++) {
+            if (tables[d][i] != -1) hits[d]++;
+        }
+    uint64_t hit = 0;
+    std::cout << esize << " ";
+    for (int d = 0; d < dim; d++) {
+        hit += hits[d];
+        std::cout << hits[d] << " ";
+    }
+    std::cout << hit << " " << tsize << " " << totaltries << " " << esize * tries << std::endl;
+
+    for (int d = 0; d < dim; d++) delete[] tables[d];
     delete[] keys;
-    std::cout << esize << " " << tsize << " " << totaltries << " " << esize * tries << std::endl;
 }
 
 int main(int argc, char **argv) {
