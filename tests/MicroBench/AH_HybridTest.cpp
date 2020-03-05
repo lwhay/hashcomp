626d700f6622cbc3e2b1146f6d8022ca985a5bbe
lwhay <lwh@whu.edu.cn>
2020-03-05 06:59:31 -0800

    testing unbalancing stdhash on microtest

--- tests/MicroBench/AH_HybridTest.cpp
+++ tests/MicroBench/AH_HybridTest.cpp
@@ -32,7 +32,65 @@ public:
     uint64_t get() { return value; }
 };
 
-typedef ConcurrentHashMap<uint64_t, /*Value **/uint64_t, std::hash<uint64_t>, std::equal_to<>> maptype;
+
+#define BIG_CONSTANT(x) (x##LLU)
+
+uint64_t MurmurHash64A(const void *key, int len, uint64_t seed) {
+    const uint64_t m = BIG_CONSTANT(0xc6a4a7935bd1e995);
+    const int r = 47;
+
+    uint64_t h = seed ^(len * m);
+
+    const uint64_t *data = (const uint64_t *) key;
+    const uint64_t *end = data + (len / 8);
+
+    while (data != end) {
+        uint64_t k = *data++;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h ^= k;
+        h *= m;
+    }
+
+    const unsigned char *data2 = (const unsigned char *) data;
+
+    switch (len & 7) {
+        case 7:
+            h ^= uint64_t(data2[6]) << 48;
+        case 6:
+            h ^= uint64_t(data2[5]) << 40;
+        case 5:
+            h ^= uint64_t(data2[4]) << 32;
+        case 4:
+            h ^= uint64_t(data2[3]) << 24;
+        case 3:
+            h ^= uint64_t(data2[2]) << 16;
+        case 2:
+            h ^= uint64_t(data2[1]) << 8;
+        case 1:
+            h ^= uint64_t(data2[0]);
+            h *= m;
+    };
+
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
+}
+
+char buf[64];
+
+struct MyHash {
+    std::size_t operator()(const uint64_t &i) const noexcept {
+        return MurmurHash64A((char *) &i, 8, 234324324231llu);
+    }
+};
+
+typedef ConcurrentHashMap<uint64_t, /*Value **/uint64_t, MyHash, std::equal_to<>> maptype;
 
 maptype *store;
 
