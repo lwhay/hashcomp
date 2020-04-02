51426b7fca916510b2a0f6741573e1135cfbed67
lwhay <lwhay@whu.edu.cn>
2020-04-02 17:36:04 +0800

    multithread brown test

--- tests/NUMA_Unit/BST_UsageTest.cpp
+++ tests/NUMA_Unit/BST_UsageTest.cpp
@@ -2,9 +2,13 @@
 // Created by Michael on 3/30/20.
 //
 
+#include <atomic>
 #include <functional>
 #include <iostream>
 #include <signal.h>
+#include <thread>
+#include <vector>
+
 #include "reclaimer_debra.h"
 #include "pool_none.h"
 #include "allocator_new.h"
@@ -16,11 +20,11 @@
 
 uint64_t thread_number = 4;
 uint64_t total_count = (1llu << 20);
-uint64_t total_round = 10;
+uint64_t timer_limit = 30;
 constexpr uint64_t NEUTRLIZE_SIGNAL = SIGQUIT;
 
 typedef bst_ns::Node<uint64_t, uint64_t> NodeType;
-typedef reclaimer_debra<NodeType, pool_none<NodeType, allocator_new<NodeType>>> Reclaimer;
+typedef reclaimer_debra<NodeType, pool_perthread_and_shared<NodeType, allocator_new<NodeType>>> Reclaimer;
 typedef allocator_new<NodeType> Allocator;
 typedef pool_perthread_and_shared<NodeType, allocator_new<NodeType>> Pool;
 typedef record_manager<Reclaimer, Allocator, Pool, NodeType> RManager;
@@ -51,7 +55,7 @@ void SimpleTest() {
     tree->initThread(0);
     std::cout << "Init: " << tracer.getRunTime() << std::endl;
 
-    for (uint64_t r = 0; r < total_round; r++) {
+    for (uint64_t r = 0; r < timer_limit; r++) {
         for (uint64_t i = 0; i < total_count; i++) {
             tree->insert(0, i, i);
             if ((i + 1) % (1llu << 20) == 0) std::cout << "\t" << i << ": " << tree->size() << std::endl;
@@ -67,12 +71,58 @@ void SimpleTest() {
     delete tree;
 }
 
+void MultiTest() {
+    Tracer tracer;
+    tracer.startTime();
+    RManager mgr(thread_number, NEUTRLIZE_SIGNAL);
+    BinaryTree *tree = new BinaryTree(-1, -1, thread_number, NEUTRLIZE_SIGNAL);
+    std::cout << "Init: " << tracer.getRunTime() << std::endl;
+    std::atomic<uint64_t> indicator{0};
+    std::vector<std::thread> threads;
+    Timer timer;
+    timer.start();
+
+    for (size_t t = 0; t < thread_number; t++) {
+        threads.push_back(std::thread([](BinaryTree *tree, size_t tid, std::atomic<uint64_t> &indicator) {
+            tree->initThread(tid);
+            while (indicator.load() == 0) {
+                for (uint64_t i = tid; i < total_count; i += thread_number) {
+                    tree->insert(tid, i, i);
+                    if ((i + 1) % (1llu << 20) == 0) std::cout << "\t" << i << ": " << tree->size() << std::endl;
+                }
+                //std::cout << "Insert" << r << ": " << tracer.getRunTime() << std::endl;
+
+                for (uint64_t i = tid; i < total_count; i += thread_number) {
+                    tree->erase(tid, i);
+                    if ((i + 1) % (1llu << 20) == 0) std::cout << "\t" << i << ": " << tree->size() << std::endl;
+                }
+                //std::cout << "Erase" << r << ": " << tracer.getRunTime() << std::endl;
+            }
+        }, tree, t, std::ref(indicator)));
+    }
+
+    while (timer.elapsedSeconds() < timer_limit) {
+        sleep(1);
+    }
+
+    indicator.store(1);
+
+    for (size_t t = 0; t < thread_number; t++) {
+        threads[t].join();
+    }
+    std::cout << "Operations: " << tracer.getRunTime() << std::endl;
+
+    delete tree;
+}
+
 int main(int argc, char **argv) {
     if (argc > 3) {
         thread_number = std::atol(argv[1]);
         total_count = std::atol(argv[2]);
-        total_round = std::atol(argv[3]);
+        timer_limit = std::atol(argv[3]);
     }
-    SimpleTest();
+    //DummyTest();
+    //SimpleTest();
+    MultiTest();
     return 0;
 }
