9e9c86d778fdf102046ba7fd418d7b39794bff6a
lwhay <lwhay@whu.edu.cn>
2020-04-22 16:04:48 +0800

    record hash

--- utils/DummyTest.cpp
+++ utils/DummyTest.cpp
@@ -122,6 +122,87 @@ void RecordScanTest() {
     delete[] records;
 }
 
+#define BIG_CONSTANT(x) (x##LLU)
+
+uint64_t MurmurHash64A(const void *key, int len, uint64_t seed) {
+    const uint64_t m = BIG_CONSTANT(0xc6a4a7935bd1e995);
+    const int r = 47;
+
+    uint64_t h = seed ^(len * m);
+
+    const uint64_t *data = (const uint64_t *) key;
+    const uint64_t *end = data + (len / 8);
+
+    while (data != end) {
+        uint64_t k = *data++;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h ^= k;
+        h *= m;
+    }
+
+    const unsigned char *data2 = (const unsigned char *) data;
+
+    switch (len & 7) {
+        case 7:
+            h ^= uint64_t(data2[6]) << 48;
+        case 6:
+            h ^= uint64_t(data2[5]) << 40;
+        case 5:
+            h ^= uint64_t(data2[4]) << 32;
+        case 4:
+            h ^= uint64_t(data2[3]) << 24;
+        case 3:
+            h ^= uint64_t(data2[2]) << 16;
+        case 2:
+            h ^= uint64_t(data2[1]) << 8;
+        case 1:
+            h ^= uint64_t(data2[0]);
+            h *= m;
+    };
+
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
+}
+
+void RecordHashTest() {
+    loads = new uint64_t[total_count];
+    std::vector<std::thread> workers;
+    RandomGenerator<uint64_t>::generate(loads, key_range, total_count, distribution_skew);
+    record **records = new record *[total_count];
+    for (uint64_t i = 0; i < total_count; i++) {
+        records[loads[i]] = new record;
+        records[loads[i]]->header1 = 0xff;
+        records[loads[i]]->key = loads[i];
+        records[loads[i]]->value = loads[i];
+    }
+    Tracer tracer;
+    tracer.startTime();
+    for (uint64_t t = 0; t < thread_number; t++) {
+        workers.push_back(std::thread([](record **records, uint64_t tid) {
+            uint64_t card = total_count / thread_number;
+            uint64_t start = tid * card;
+            for (uint64_t i = start; i < start + card; i++) {
+                uint64_t hash = MurmurHash64A((void *) loads[i], sizeof(uint64_t), 0x234233242324323) % total_count;
+                value += records[hash]->value;
+            }
+        }, records, t));
+    }
+
+    for (uint64_t t = 0; t < thread_number; t++) workers[t].join();
+    total_time.fetch_add(tracer.getRunTime());
+
+    std::cout << "Recordseq Tpt: " << (double) total_count * thread_number / total_time.load() << std::endl;
+    for (uint64_t i = 0; i < total_count; i++) delete records[i];
+    delete[] records;
+}
+
 int main(int argc, char **argv) {
     if (argc > 4) {
         thread_number = std::atol(argv[1]);
@@ -132,6 +213,7 @@ int main(int argc, char **argv) {
     RecordTest();
     RecordPtrTest();
     RecordScanTest();
+    RecordHashTest();
 
     std::bitset<128> bs(0);
     for (int i = 0; i < 128; i++) if (i % 2 == 0) bs.set(i);
