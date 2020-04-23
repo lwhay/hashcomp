b0f0ced982de7a61aeec33fa812a695db8c24b8a
lwhay <lwhay@whu.edu.cn>
2020-04-23 20:45:11 +0800

    add sparsemap and boostmap

--- utils/GeneralMapTest.cpp
+++ utils/GeneralMapTest.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <sparsehash/dense_hash_map>
+#include <sparsehash/sparse_hash_map>
+#include <boost/unordered_map.hpp>
 #include "tracer.h"
 
 uint64_t key_range = (1llu << 20);
@@ -15,7 +17,7 @@ uint64_t total_round = 10;
 
 double distribution = .0f;
 
-uint64_t map_type = 2; // 0/1: unordered_map with same/random order; 2/3: dense_hash_map with random/random order
+uint64_t map_type = 5; // 0/1: unordered_map with same/random order; 2/3: dense_hash_map with random/random order
 
 uint64_t *loads;
 
@@ -40,17 +42,52 @@ void densemaptest(bool random) {
     loads = (uint64_t *) calloc(total_count, sizeof(uint64_t));
     RandomGenerator<uint64_t>::generate(loads, key_range, total_count, distribution);
     google::dense_hash_map<uint64_t, uint64_t> map;
+    map.set_empty_key(-1);
     uint64_t value = 0;
     Tracer tracer;
     tracer.startTime();
     for (uint64_t i = 0; i < total_count; i++) map.insert(std::pair<uint64_t, uint64_t>(loads[i], loads[i]));
-    std::cout << "Google densesemap insert: " << (double) total_count / tracer.getRunTime() << std::endl;
+    std::cout << "Google densemap insert: " << (double) total_count / tracer.getRunTime() << std::endl;
     if (random) std::random_shuffle(loads, loads + total_count);
     std::cout << "Google densesemap shuffle: " << (double) total_count / tracer.getRunTime() << std::endl;
     for (int r = 0; r < 10; r++) for (uint64_t i = 0; i < total_count; i++) value = map.find(loads[i])->second;
     std::cout << "Google densesemap find: " << (double) total_count * total_round / tracer.getRunTime() << std::endl;
     for (int r = 0; r < 10; r++) for (uint64_t i = 0; i < total_count; i++) map.find(loads[i])->second = loads[i];
     std::cout << "Google densesemap find: " << (double) total_count * total_round / tracer.getRunTime() << std::endl;
+}
+
+void sparsemaptest(bool random) {
+    loads = (uint64_t *) calloc(total_count, sizeof(uint64_t));
+    RandomGenerator<uint64_t>::generate(loads, key_range, total_count, distribution);
+    google::sparse_hash_map<uint64_t, uint64_t> map;
+    uint64_t value = 0;
+    Tracer tracer;
+    tracer.startTime();
+    for (uint64_t i = 0; i < total_count; i++) map.insert(std::pair<uint64_t, uint64_t>(loads[i], loads[i]));
+    std::cout << "Google densemap insert: " << (double) total_count / tracer.getRunTime() << std::endl;
+    if (random) std::random_shuffle(loads, loads + total_count);
+    std::cout << "Google densesemap shuffle: " << (double) total_count / tracer.getRunTime() << std::endl;
+    for (int r = 0; r < 10; r++) for (uint64_t i = 0; i < total_count; i++) value = map.find(loads[i])->second;
+    std::cout << "Google densesemap find: " << (double) total_count * total_round / tracer.getRunTime() << std::endl;
+    for (int r = 0; r < 10; r++) for (uint64_t i = 0; i < total_count; i++) map.find(loads[i])->second = loads[i];
+    std::cout << "Google densesemap find: " << (double) total_count * total_round / tracer.getRunTime() << std::endl;
+}
+
+void boostmaptest(bool random) {
+    loads = (uint64_t *) calloc(total_count, sizeof(uint64_t));
+    RandomGenerator<uint64_t>::generate(loads, key_range, total_count, distribution);
+    boost::unordered_map<uint64_t, uint64_t> map;
+    uint64_t value = 0;
+    Tracer tracer;
+    tracer.startTime();
+    for (uint64_t i = 0; i < total_count; i++) map.insert(std::pair<uint64_t, uint64_t>(loads[i], loads[i]));
+    std::cout << "Boost unorderedmap insert: " << (double) total_count / tracer.getRunTime() << std::endl;
+    if (random) std::random_shuffle(loads, loads + total_count);
+    std::cout << "Boost unorderedmap shuffle: " << (double) total_count / tracer.getRunTime() << std::endl;
+    for (int r = 0; r < 10; r++) for (uint64_t i = 0; i < total_count; i++) value = map.find(loads[i])->second;
+    std::cout << "Boost unorderedmap find: " << (double) total_count * total_round / tracer.getRunTime() << std::endl;
+    for (int r = 0; r < 10; r++) for (uint64_t i = 0; i < total_count; i++) map.find(loads[i])->second = loads[i];
+    std::cout << "Boost unorderedmap update: " << (double) total_count * total_round / tracer.getRunTime() << std::endl;
 }
 
 int main(int argc, char **argv) {
@@ -74,7 +111,19 @@ int main(int argc, char **argv) {
         case 3:
             densemaptest(true);
             break;
+        case 4:
+            sparsemaptest(false);
+            break;
+        case 5:
+            sparsemaptest(true);
+            break;
+        case 6:
+            boostmaptest(false);
+            break;
+        case 7:
+            boostmaptest(true);
+            break;
         default:
-            std::cout << "Type 0|1|2|3" << std::endl;
+            std::cout << "Type 0|1|2|3|4|5" << std::endl;
     }
 }
