b76a31cbc72bb8d4c677f95f138b30bffa2d4cb6
lwhay <lwh@whu.edu.cn>
2020-10-19 06:21:00 -0700

    producer/consumer

--- utils/ProducerTest.cpp
+++ utils/ProducerTest.cpp
@@ -7,6 +7,7 @@
 #include <atomic>
 #include <iostream>
 #include <semaphore.h>
+#include "tracer.h"
 
 #include <mutex>
 #include <condition_variable>
@@ -39,8 +40,9 @@ private:
 
 using namespace std;
 
-constexpr int THREAD_NUMBER = 8;
-constexpr int TOTAL_COUNTER = (1 << 12);
+int THREAD_NUMBER = 8;
+int TOTAL_COUNTER = (1 << 22);
+long SLEEP_PERIOD = ((1llu << 36) / TOTAL_COUNTER);
 
 atomic<unsigned long long> mark(1);
 
@@ -55,7 +57,6 @@ void naiveTest() {
         active[i].store(false);
         full[i].store(false);
         arraytrd.push_back(thread([](atomic<bool> *active, atomic<bool> *full, int tid) {
-            cout << tid << endl;
             unsigned long long count = 0;
             while (true) {
                 full->store(true);
@@ -73,16 +74,18 @@ void naiveTest() {
     // Consumer
     for (int i = 0; i < THREAD_NUMBER; i++) {
         arraytrd.push_back(thread([](atomic<bool> *active, atomic<bool> *full, int tid) {
-            cout << tid << endl;
+            Tracer tracer;
+            tracer.startTime();
             unsigned long long count = 0;
             while (true) {
-                this_thread::sleep_for(chrono::nanoseconds(10000000));
+                this_thread::sleep_for(chrono::nanoseconds(SLEEP_PERIOD));
                 // if (count == TOTAL_COUNTER) break;
                 if (full->load() /*&& count % 10 == 0*/) {
                     active->store(true);
                 }
-                if (tid % THREAD_NUMBER == 0)
-                    cout << tid << ":" << count << ":" << full->load() << ":" << active->load() << endl;
+                if (tid % THREAD_NUMBER == 0 && count % ((2llu << 28) / SLEEP_PERIOD) == 0)
+                    cout << tid << ":" << count << ":" << full->load() << ":" << active->load() << ":"
+                         << tracer.getRunTime() << endl;
                 count++;
                 if (term.load()) break;
             }
@@ -106,7 +109,6 @@ void csemTest() {
         sem_init(active + i, 0, 1);
         full[i].store(false);
         arraytrd.push_back(thread([](sem_t *active, atomic<bool> *full, int tid) {
-            cout << tid << endl;
             unsigned long long count = 0;
             while (true) {
                 full->store(true);
@@ -120,16 +122,17 @@ void csemTest() {
     // Consumer
     for (int i = 0; i < THREAD_NUMBER; i++) {
         arraytrd.push_back(thread([](sem_t *active, atomic<bool> *full, int tid) {
-            cout << tid << endl;
+            Tracer tracer;
+            tracer.startTime();
             unsigned long long count = 0;
             while (true) {
-                this_thread::sleep_for(chrono::nanoseconds(10000000));
+                this_thread::sleep_for(chrono::nanoseconds(SLEEP_PERIOD));
                 // if (count == TOTAL_COUNTER) break;
                 if (full->load() /*&& count % 10 == 0*/) {
                     sem_post(active);
                 }
-                if (tid % THREAD_NUMBER == 0)
-                    cout << tid << ":" << count << ":" << full->load() << endl;
+                if (tid % THREAD_NUMBER == 0 && count % ((2llu << 28) / SLEEP_PERIOD) == 0)
+                    cout << tid << ":" << count << ":" << full->load() << ":" << tracer.getRunTime() << endl;
                 count++;
                 if (term.load()) break;
             }
@@ -152,7 +155,6 @@ void psemTest() {
     for (int i = 0; i < THREAD_NUMBER; i++) {
         full[i].store(false);
         arraytrd.push_back(thread([](Semaphore *active, atomic<bool> *full, int tid) {
-            cout << tid << endl;
             unsigned long long count = 0;
             while (true) {
                 full->store(true);
@@ -166,16 +168,17 @@ void psemTest() {
     // Consumer
     for (int i = 0; i < THREAD_NUMBER; i++) {
         arraytrd.push_back(thread([](Semaphore *active, atomic<bool> *full, int tid) {
-            cout << tid << endl;
+            Tracer tracer;
+            tracer.startTime();
             unsigned long long count = 0;
             while (true) {
-                this_thread::sleep_for(chrono::nanoseconds(10000000));
+                this_thread::sleep_for(chrono::nanoseconds(SLEEP_PERIOD));
                 // if (count == TOTAL_COUNTER) break;
                 if (full->load() /*&& count % 10 == 0*/) {
                     active->notify();
                 }
-                if (tid % THREAD_NUMBER == 0)
-                    cout << tid << ":" << count << ":" << full->load() << endl;
+                if (tid % THREAD_NUMBER == 0 && count % ((2llu << 28) / SLEEP_PERIOD) == 0)
+                    cout << tid << ":" << count << ":" << full->load() << ":" << tracer.getRunTime() << endl;
                 count++;
                 if (term.load()) break;
             }
@@ -190,9 +193,35 @@ void psemTest() {
     cout << mark << endl;
 }
 
-int main() {
-    // naiveTest();
-    // csemTest();
-    psemTest();
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+        cout << "command 0|1|2 (0: naive; 1: c's semaphore; 2: cpp's semaphore)" << endl;
+        exit(-1);
+    }
+    if (argc > 2) {
+        THREAD_NUMBER = std::atoi(argv[2]);
+        TOTAL_COUNTER = std::atol(argv[3]);
+        SLEEP_PERIOD = ((1llu << 36) / TOTAL_COUNTER);
+    }
+    switch (std::atoi(argv[1])) {
+        case 0:
+            cout << "Naive " << argv[1] << " THREAD_NUM: " << THREAD_NUMBER << " TOTAL_COUNT: " << TOTAL_COUNTER
+                 << " SLEEP_PERIOD: " << SLEEP_PERIOD << endl;
+            naiveTest();
+            break;
+        case 1:
+            cout << "CSem" << argv[1] << " THREAD_NUM: " << THREAD_NUMBER << " TOTAL_COUNT: " << TOTAL_COUNTER
+                 << " SLEEP_PERIOD: " << SLEEP_PERIOD << endl;
+            csemTest();
+            break;
+        case 2:
+            cout << "PSem" << argv[1] << " THREAD_NUM: " << THREAD_NUMBER << " TOTAL_COUNT: " << TOTAL_COUNTER
+                 << " SLEEP_PERIOD: " << SLEEP_PERIOD << endl;
+            psemTest();
+            break;
+        default:
+            cout << "command 0|1|2 (0: naive; 1: c's semaphore; 2: cpp's semaphore)" << endl;
+            exit(-1);
+    }
     return 0;
 }
